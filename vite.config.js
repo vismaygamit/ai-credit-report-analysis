9dbf6b04781439d9a6f92a6541f64dcebe2894b9
Vismay Gamit <42162523+vismaygamit@users.noreply.github.com>
2025-09-01 23:52:14 +0530

    sitemap by npm pkg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://ai-credit-report-analysis.vercel.app", // replace with your domain
+      routes: ["/", "/privacypolicy"],
+    }),
+    tailwindcss(),
+  ],
 });
